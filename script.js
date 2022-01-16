df54b9bd0967a53c9f90982369f6bd89aaa060d4
Isabel Barbosa <isabelbarbosa.bel@gmail.com>
2022-01-16 17:03:50 -0300

    Obter três personagens no Front-end

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status');
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-        method:'GET',
-        headers: {
-            Accept: 'application/json',
-            "Content-type": 'application/json'
-        }
-    }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
-        imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.species;
-        condicao.innerHTML = data.status;        
-    });
+    for(let x=1; x<=3;x++) {
+        let numeroAleatorio = gerarValorAleatorio();
+        const nomeDoPersonagem = document.querySelector(`#nome${x}`);
+        const especie = document.querySelector(`#especie${x}`);
+        const condicao = document.querySelector(`#status${x}`);
+        const imagem = document.querySelector(`#img${x}`);
+        fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+            method:'GET',
+            headers: {
+                Accept: 'application/json',
+                "Content-type": 'application/json'
+            }
+        }).then((response) => response.json()).then((data) => {
+            imagem.src = data.image;
+            imagem.alt = data.name;
+            nomeDoPersonagem.innerHTML = data.name;
+            especie.innerHTML = data.species;
+            condicao.innerHTML = data.status;        
+        });
+    }
+    
 }
 botao.onclick = pegarPersonagem;
